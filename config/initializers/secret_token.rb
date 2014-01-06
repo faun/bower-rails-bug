028ce2424bc497130f47755ddad1c4c9cbf43acd
Faun <faun@riseup.net>
2014-01-06 14:32:26 +0700

    Init project with angular assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BowerRailsBug::Application.config.secret_token = '701dff503bbd8a7e1e6a7af71a14408464c35fee35f08628a35380258558b62c2c1f5b690cdf721403fe787fecb453573253ab141d56e824154d0dc4a5ae3099'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BowerRailsBug::Application.config.session_store :cookie_store, key: '_bower-rails-bug_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BowerRailsBug::Application.config.session_store :active_record_store
